9de65a7858e0a9ffd4d06ca544b494cfe86f51dc
Nahuel <nahuel.profesor@gmail.com>
2023-05-11 23:58:25 -0300

    actualizando el store, completado

--- src/users/store/users-store.js
+++ src/users/store/users-store.js
@@ -7,7 +7,12 @@ const state = {
 }
 
 const loadNextPage = async() => {
-    await loadUsersByPage(state.currentPage + 1)
+    const users =await loadUsersByPage(state.currentPage + 1);
+    if(users.length === 0) return;
+
+    state.currentPage += 1;
+    state.users = users;
+
 }
 
 const loadPreviousPage = async() => {
@@ -28,6 +33,6 @@ export default {
     onUserChanged,
     reloadPage,
 
-    getUser: () => [...state.users],
+    getUsers: () => [...state.users],
     getCurrenPage: () => state.currentPage,
 }
--- src/users/users-app.js
+++ src/users/users-app.js
@@ -7,4 +7,6 @@ import usersStore from "./store/users-store";
 export const UsersApp = async(element) => {
     element.innerHTML = 'Loading...';
     await usersStore.loadNextPage();
+
+    console.log(usersStore.getUsers());
 }
