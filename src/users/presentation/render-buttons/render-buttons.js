096ba861094b3e03dcd66252e664edd29f493d96
Nahuel <nahuel.profesor@gmail.com>
2023-07-27 22:13:10 -0300

    avanzando con la funcionalidad de los botones

--- src/users/presentation/render-buttons/render-buttons.js
+++ src/users/presentation/render-buttons/render-buttons.js
@@ -1,4 +1,5 @@
 import usersStore from '../../store/users-store'
+import { renderTable } from '../render-table/render-table';
 import './render-buttons.css';
 
 /**
@@ -19,4 +20,14 @@ export const renderButtons = ( element ) => {
     
     element.append( prevButton, currentPageLabel, nextButton );
 
+    nextButton.addEventListener('click', async() => {
+        await usersStore.loadNextPage();
+        currentPageLabel.innerText = usersStore.getCurrenPage();
+        renderTable(element);
+    });
+
+    
+    prevButton.addEventListener('click', () => {
+        
+    });
 }
