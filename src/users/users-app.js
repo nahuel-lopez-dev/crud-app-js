d6f7af687559b147bc718a20a717954b04be9c9f
Nahuel <nahuel.profesor@gmail.com>
2023-07-29 21:22:47 -0300

    botón flotante agregado

--- src/users/users-app.js
+++ src/users/users-app.js
@@ -1,3 +1,4 @@
+import { renderAddButton } from "./presentation/render-add-button/render-add-button";
 import { renderButtons } from "./presentation/render-buttons/render-buttons";
 import { renderTable } from "./presentation/render-table/render-table";
 import usersStore from "./store/users-store";
@@ -13,4 +14,5 @@ export const UsersApp = async(element) => {
 
     renderTable(element);
     renderButtons(element);
+    renderAddButton(element)
 }
