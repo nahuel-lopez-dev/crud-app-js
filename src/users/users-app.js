ed89faa7c66f3c1028542b5333ad490072a7f2f8
Nahuel <nahuel.profesor@gmail.com>
2023-07-30 10:12:02 -0300

    modal creado manualmente

--- src/users/users-app.js
+++ src/users/users-app.js
@@ -1,7 +1,8 @@
+import usersStore from "./store/users-store";
 import { renderAddButton } from "./presentation/render-add-button/render-add-button";
 import { renderButtons } from "./presentation/render-buttons/render-buttons";
+import { renderModal } from "./presentation/render-modal/render-modal";
 import { renderTable } from "./presentation/render-table/render-table";
-import usersStore from "./store/users-store";
 
 /**
  * 
@@ -14,5 +15,6 @@ export const UsersApp = async(element) => {
 
     renderTable(element);
     renderButtons(element);
-    renderAddButton(element)
+    renderAddButton(element);
+    renderModal(element);
 }
